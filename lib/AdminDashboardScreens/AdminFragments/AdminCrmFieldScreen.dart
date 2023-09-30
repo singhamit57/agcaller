365728a5bda5adf08064506593e059d7b0bfa4a7
Amit <singhamit7052770205@gmail.com>
2023-09-30 18:30:45 +0530

    ten commit

--- lib/AdminDashboardScreens/AdminFragments/AdminCrmFieldScreen.dart
+++ lib/AdminDashboardScreens/AdminFragments/AdminCrmFieldScreen.dart
@@ -30,61 +30,130 @@ class _AdminCrmFieldScreenState extends State<AdminCrmFieldScreen>
           style: TextStyle(color: whiteColors),
         ),
       ),
-      body:Column(
-        children: [
-          SizedBox(height: 15,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(height: 10,),
-              Text("Preview",style: TextStyle(color: logoColors),),
-              SizedBox(width: 10,),
-              Text("Import",style: TextStyle(color: logoColors),),
-              SizedBox(width: 10,)
-            ],
-          ),
-          SizedBox(height: 30,),
-          Padding(
-            padding: EdgeInsets.only(left: 10,right: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 15,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text("DEFAULT FIELDS*"),
-                Icon(Icons.navigate_next,)
+                SizedBox(height: 10,),
+                Text("Preview",style: TextStyle(color: logoColors),),
+                SizedBox(width: 10,),
+                Text("Import",style: TextStyle(color: logoColors),),
+                SizedBox(width: 10,)
               ],
             ),
-          ),
-          SizedBox(height: 40,),
-          Padding(
-            padding: EdgeInsets.only(left: 10,right: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("FIXED FIELD*"),
-                Icon(Icons.navigate_next,)
-              ],
+            SizedBox(height: 30,),
+            Padding(
+              padding: EdgeInsets.only(left: 10,right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("DEFAULT FIELDS*"),
+                  Icon(Icons.navigate_next,)
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 45,),
-          Padding(
-            padding: EdgeInsets.only(left: 10,right: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("CUSTOM FIELD*"),
-                Row(
-                  children: [
-                    Icon(Icons.add,color: logoColors,),
-                    Text("Add Field",style: TextStyle(color: logoColors),)
-                  ],
-                ),
+            SizedBox(height: 40,),
+            Padding(
+              padding: EdgeInsets.only(left: 10,right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("FIXED FIELD*"),
+                  Icon(Icons.navigate_next,)
+                ],
+              ),
+            ),
+            SizedBox(height: 45,),
+            Padding(
+              padding: EdgeInsets.only(left: 10,right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("CUSTOM FIELD*"),
+                  Row(
+                    children: [
+                      Icon(Icons.add,color: logoColors,),
+                      Text("Add Field",style: TextStyle(color: logoColors),)
+                    ],
+                  ),
 
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 30,),
-          Text("*These fields are completely customizable",style: TextStyle(fontSize: 10,fontWeight: FontWeight.w300),)
-        ],
+            SizedBox(height: 30,),
+            Container(
+              alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(left: 10),
+                child: Text("*These fields are completely customizable",style: TextStyle(fontSize: 10,fontWeight: FontWeight.w300),)),
+            Container(
+              height: 60,
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10,right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Option"),
+                          Text("Source"),
+                        ],
+                      ),
+
+                      Icon(Icons.text_rotation_none_outlined,color: logoColors,)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10,),
+            Icon(Icons.expand_circle_down,color: logoColors,),
+            SizedBox(height: 20,),
+            Container(
+              height: 60,
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10,right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Option"),
+                          Text("Status"),
+                        ],
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Icon(Icons.text_rotation_none_outlined,color: logoColors,),
+                            SizedBox(width: 10,),
+                            Icon(Icons.lock_outline,color: logoColors,),
+                            SizedBox(width: 10,),
+                            Icon(Icons.menu_open,color: logoColors,),
+                            SizedBox(width: 10,),
+
+                          ],
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10,),
+            Icon(Icons.expand_circle_down,color: logoColors,),
+            SizedBox(height: 50,),
+          ],
+        ),
       ),
     );
   }
--- lib/AdminDashboardScreens/AdminFragments/AdminMenuScreen.dart
+++ lib/AdminDashboardScreens/AdminFragments/AdminMenuScreen.dart
@@ -1,6 +1,8 @@
 import 'package:agcaller/constants/const_colors.dart';
 import 'package:flutter/material.dart';
 
+import '../AdminMenuScreensPages/AdminRecurringFollowupScreen.dart';
+
 class AdminMenuScreen extends StatefulWidget
 {
   const AdminMenuScreen({Key? key}):super(key: key);
@@ -20,6 +22,111 @@ class _AdminMenuScreenScreenState extends State<AdminMenuScreen>
           "Menu",
           style: TextStyle(color: whiteColors),
         ),
+      ),
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: (){
+             Navigator.push(context, MaterialPageRoute(builder: (context)=>AdminRecurringFollowupScreen()));
+            },
+            child: Card(
+              elevation: 0.0,
+              color: backgroundsColors,
+              child: Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(left: 15,top: 10),
+                child: Row(
+                  children: [
+                    Icon(Icons.watch_later_outlined,color: logoColors,),
+                    SizedBox(width: 30,),
+                    Text("Configure recurring followup"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xffd2d0d0),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 15,top: 10),
+            child: Row(
+              children: [
+                Icon(Icons.calendar_today,color: logoColors,),
+                SizedBox(width: 30,),
+                Text("Acticity Logs"),
+              ],
+            ),
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xffd2d0d0),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 15,top: 10),
+            child: Row(
+              children: [
+                Icon(Icons.vpn_key,color: logoColors,),
+                SizedBox(width: 30,),
+                Text("Change Password"),
+              ],
+            ),
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xffd2d0d0),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 15,top: 10),
+            child: Row(
+              children: [
+                Icon(Icons.privacy_tip_outlined,color: logoColors,),
+                SizedBox(width: 30,),
+                Text("Privacy Policy"),
+              ],
+            ),
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xffd2d0d0),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 15,top: 10),
+            child: Row(
+              children: [
+                Icon(Icons.no_accounts_outlined,color: logoColors,),
+                SizedBox(width: 30,),
+                Text("Delete Account"),
+              ],
+            ),
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xffd2d0d0),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 15,top: 10),
+            child: Row(
+              children: [
+                Icon(Icons.login,color: logoColors,),
+                SizedBox(width: 30,),
+                Text("Logout"),
+              ],
+            ),
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xffd2d0d0),
+          ),
+          SizedBox(height: 10,),
+          Text("Version 3.6.14-2",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w200),),
+        ],
       ),
     );
   }
